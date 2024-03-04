90d689938a77fdb322f7597d6ff98d65ad1a9bf8
Ygor <ygorpb2@gmail.com>
2024-03-04 12:27:37 -0300

    Fix-imgLine

--- src/shared/components/UILineDivision.tsx
+++ src/shared/components/UILineDivision.tsx
@@ -18,7 +18,7 @@ const UILineDivision = ({ maxwidth, color, height, mbottom, mtop }: UILineDivisi
 }
 export const LineDivision = styled.div < { maxwidth: number, height: number, color: string, mtop: number, mbottom: number }>`
   background: ${(props: UILineDivisionProps) => (props.color ? `#${props.color}` : "#999999")};
-  max-width: ${(props: any) => (props.maxwidth ? `${props.maxwidth}px` : "905px")};
+  max-width: ${(props: any) => (props.maxwidth > 0 ? `${props.maxwidth}px` : "100%")};
   width: 100%;
   height:${(props: UILineDivisionProps) => (props.height ? `${props.height}px` : "1px")};
   margin-bottom: ${(props: UILineDivisionProps) => (props.mbottom ? `${props.mbottom}px` : "21px")};
