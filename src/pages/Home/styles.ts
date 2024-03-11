92639963acbf511484b71be114315f24e5b876e8
Ygor <ygorpb2@gmail.com>
2024-03-11 09:19:01 -0300

    update

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -72,7 +72,7 @@ export const HomeImgContent = styled.div`
 `;
 
 export const HomeCard = styled.div`
-  width: 287.36px;
+  width: 286.36px;
   color: #333333;
   height: 285px;
   padding: 10px 11px 10px 11px;
